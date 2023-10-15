9fb8945c2ade16f4562bfbda494d61a68ca722d2
Philip Chmielowiec <67855069+philipc2@users.noreply.github.com>
2023-10-15 15:09:32 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,16 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
-
 # -- General configuration
-
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
